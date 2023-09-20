1671bb34441f8394b83a1a49665ef050bbe34412
Mingwei Zhu <mzhu1@terpmail.umd.edu>
2023-09-20 00:37:22 -0400

    [proposal] phase more ...

--- src/Qafny/Runner.hs
+++ src/Qafny/Runner.hs
@@ -1,9 +1,6 @@
 module Qafny.Runner where
 import           Control.Algebra                 (run)
-import qualified Control.Carrier.Error.Either    as ErrE (runError)
-
 import           Control.Carrier.Reader          (runReader)
-import           Control.Carrier.State.Strict    (runState)
 import           Control.Carrier.Trace.Returning (runTrace)
 import           Qafny.Codegen                   (codegenAST)
 import           Qafny.Config                    (Configs)
