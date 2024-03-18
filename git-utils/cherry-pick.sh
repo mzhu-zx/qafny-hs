edbc81b85cabf689b1d57a9d3cdfd1407f823bc5
Mingwei Zhu <mzhu1@terpmail.umd.edu>
2024-03-18 13:54:53 -0400

    [git] fix test

--- git-utils/cherry-pick.sh
+++ git-utils/cherry-pick.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-if [[ -s $1 || -s $2 ]]; then
+if [[ -z "$1" || -z "$2" ]]; then
     echo "Usage: cherry pick from [1] to [2]"
+    exit -1
 fi
 
 git cherry-pick --strategy=ort --strategy-option=theirs $1...$2
