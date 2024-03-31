0f603e845421ef5089849373f1fd6af3ec715d00
Mingwei Zhu <mzhu1@terpmail.umd.edu>
2024-03-31 15:39:53 -0400

    [scripts] add git-diff script, re-organize scripts

---
+++ git-utils/diff.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [[ -z "$1" || -z "$2" ]]; then
+    echo "Usage: diff examples between [1] and [2]"
+    exit -1
+fi
+
+git diff $1..$2 -- . ':!src/' ':!ancilla/' ':!app/' ':!git-utils' ':!notes/' ':!test/'
