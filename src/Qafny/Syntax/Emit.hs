a6c46f9fc8903978896bb1e0f125ebc326269836
Mingwei Zhu <mzhu1@terpmail.umd.edu>
2024-04-09 22:52:34 -0400

    [pretty] `PrettyPrinter` wip

--- src/Qafny/Syntax/Emit.hs
+++ src/Qafny/Syntax/Emit.hs
@@ -7,42 +7,32 @@
 
 module Qafny.Syntax.Emit where
 
-import           Control.Arrow
-    (Arrow (first))
-import           Control.Monad.Reader
-import           Data.Array.IArray
-    (Array, array, (!))
-import qualified Data.Map.Strict          as Map
-import           Data.Maybe
-    (maybeToList)
+import qualified Data.Map.Strict                         as Map
 import           Data.Sum
-import qualified Data.Text                as TS
-import           Data.Text.Lazy
-    (Text, unpack)
-import           Data.Text.Lazy.Builder
-    (fromString)
-import qualified Data.Text.Lazy.Builder   as TB
-import qualified Data.Text.Lazy.IO        as TIO
-    (putStrLn)
+
+-- Text
+import qualified Data.Text                               as TS
+import qualified Data.Text.Lazy                          as TL
+
+-- Qafny
 import           Qafny.Syntax.AST
 import           Qafny.Syntax.EmitBinding
 import           Qafny.Syntax.IR
+import           Qafny.Syntax.Render
 
-import           Data.Void
-    (Void)
-
+-- PP
 import           Prettyprinter
-    (line, space, lbracket, rbracket, lbrace, rbrace)
-import qualified Prettyprinter            as P
+    (lbrace, rbrace, space)
+import qualified Prettyprinter                           as P
 
 -------------------- Builder --------------------
-type Builder = P.Doc Void
+type Builder = P.Doc TS.Text
 
+viaShow :: Show e => e -> Builder
+viaShow = P.viaShow
 
--- indentTable :: Array Int TB.Builder
--- indentTable =
---   array (0, 20) [ (x, TB.fromText $ TS.replicate x (TS.singleton ' '))
---                 | x <- [ 0 .. 20 ] ]
+line :: Builder
+line = P.line
 
 incr2 :: DafnyPrinter a => a -> Builder
 incr2 = P.indent 2 . pp
@@ -56,62 +46,31 @@ encloseSep
 encloseSep l r s bs =
   P.encloseSep (pp l) (pp r) (pp s) (pp <$> bs)
 
--- indent :: Builder
--- indent = do
---   (n, _) <- ask
---   pp $ indentTable ! n
+parens :: DafnyPrinter a => a -> Builder
+parens = P.parens . pp
 
--- withParen :: DafnyPrinter a => a -> Builder
--- withParen b = pp '(' <> pp b <> pp ')'
+brackets :: DafnyPrinter a => a -> Builder
+brackets = P.brackets . pp
 
--- withBracket :: Builder -> Builder
--- withBracket b = pp '[' <> b <> pp ']'
+braces :: DafnyPrinter a => a -> Builder
+braces = P.braces . pp
 
--- withBrace :: Builder -> Builder
--- withBrace b =
---   indent <> pp "{"
---   <> line <!> b <> line
---   <> indent <!> "}"
---   <!> line
+byComma :: DafnyPrinter a => [a] -> Builder
+byComma = P.cat . P.punctuate P.comma . (pp <$>)
 
-
--- | Elements separate by 'op' but without leading or trailing separator
-by :: (DafnyPrinter b, DafnyPrinter a) => b -> [a] -> Builder
-by op []     = mempty
-by op (x:xs) = foldl (\ys y -> ys <!> op <!> pp y) (pp x) xs
-
--- byComma :: DafnyPrinter a => [a] -> Builder
--- byComma = by ", "
+list :: DafnyPrinter a => [a] -> Builder
+list = P.list . (pp <$>)
 
 tupled :: DafnyPrinter a => [a] -> Builder
-tupled = P.tupled . (pp <$>)  
+tupled = P.tupled . (pp <$>)
 
--- -- | Pp and separate by line but with a trailing newline
--- byLineT :: DafnyPrinter a => [a] -> Builder
--- byLineT a = by (line <> indent) a <!> line
-
--- -- | Pp each element and separate them by a newline without producing any
--- -- newline in the end but with a leading newline if the list is nonempty
--- byLineL :: DafnyPrinter a => [a] -> Builder
--- byLineL a = lineHuh a <> by line a
-
--- lineHuh :: Foldable t => t a -> Builder
--- lineHuh a = if null a then mempty else line
-
--- FIXME : raise an error if a DEBUG mode term is emitted
 debugOnly' :: DafnyPrinter a => a -> Builder
 debugOnly' a = do
-  (_, b) <- ask
-  if b
-    then pp a
-    else pp "/* (DEBUG) */"
+  P.annotate (TS.pack "/* (DEBUG) */") (pp a)
 
 debugOnly :: (Show e, DafnyPrinter a) => e -> a -> Builder
-debugOnly e d = do
-  (_, b) <- ask
-  if b
-    then pp d
-    else "// (DEBUG)" <+> show e
+debugOnly e a = do
+  P.annotate (TS.pack "/* (DEBUG)" <> TS.pack (show e) <> TS.pack "*/") (pp a)
 
 infixr 6 <!>
 
@@ -123,7 +82,7 @@ a <!> b = pp a <> pp b
 {-# INLINE (<!>) #-}
 
 (<+>) :: (DafnyPrinter a, DafnyPrinter b) => a -> b -> Builder
-a <+> b = a <!> space <!> b
+a <+> b = pp a P.<+> pp b
 {-# INLINE (<+>) #-}
 
 
@@ -144,14 +103,14 @@ instance DafnyPrinter AST where
   pp = vsep
 
 instance DafnyPrinter Ty where
-  pp TNat               = pp "nat"
-  pp TInt               = pp "int"
-  pp TBool              = pp "bool"
-  pp (TArrow tys ty)    = tupled tys <+> "->" <+> ty
-  pp TMeasured          = debugOnly' $ pp "measured"
-  pp (TQReg n)          = debugOnly' $ "qreg" <+> n
-  pp (TSeq ty)          = "seq<" <!> ty <!> ">"
-  pp (TEmit (TAny s))    = pp s
+  pp TNat             = pp "nat"
+  pp TInt             = pp "int"
+  pp TBool            = pp "bool"
+  pp (TArrow tys ty)  = tupled tys <+> "->" <+> ty
+  pp TMeasured        = debugOnly' $ pp "measured"
+  pp (TQReg n)        = debugOnly' $ "qreg" <+> n
+  pp (TSeq ty)        = "seq<" <!> ty <!> ">"
+  pp (TEmit (TAny s)) = pp s
 
 instance DafnyPrinter MethodType where
   pp ty@MethodType {mtSrcParams=ts, mtSrcReturns=ts'} = debugOnly ty $
@@ -160,12 +119,12 @@ instance DafnyPrinter MethodType where
 instance DafnyPrinter MethodElem where
   pp tt = debugOnly tt $ ppSub tt
     where
-      ppSub (MTyPure x ty)   = debugOnly tt $ fromString x <+> ":" <+> ty
-      ppSub (MTyQuantum x e) = fromString x <!> "[" <!> e <!> "]"
+      ppSub (MTyPure x ty)   = debugOnly tt $ x <+> ":" <+> ty
+      ppSub (MTyQuantum x e) = x <!> "[" <!> e <!> "]"
 
 instance DafnyPrinter AExp where
   pp (ANat n) = pp n
-  pp (AVar v) = pp $ fromString v
+  pp (AVar v) = pp v
 
 instance DafnyPrinter QTy where
   pp TNor  = pp "nor"
@@ -181,61 +140,67 @@ instance DafnyPrinter QDafny where
   pp (QDafny s) = pp s
 
 instance DafnyPrinter (QMethod ()) where
-  pp (QMethod idt bds rets reqs ens blockHuh) =
-    "method" <+> idt <+> tupled bds <> ppRets rets
-    <!> lineHuh reqEns
-    <!> (withIncr2 . by line $ (indent <!>) <$> reqEns)
-    -- add a tailing newline if we have conds and blocks
-    <!> lineHuh blockHuh
-    <!> by line (maybeToList blockHuh)
-    where ppRets [] = mempty
-          ppRets r  = " returns" <+> withParen (byComma r)
-          reqEns = ppConds "requires" reqs <> ppConds "ensures" ens
+  pp (QMethod idt bds rets reqs ens blockMaybe) =
+    "method" <+> idt <+> tupled bds <+> ppRets rets
+    <!> incr2 (vsep reqEns)
+    <!> maybe mempty pp blockMaybe
+    where
+      ppRets [] = mempty
+      ppRets x  = tupled x
+      reqEns =
+        (("requires" <+>) <$> reqs) <>
+        (("ensures"  <+>) <$> ens)
 
 instance DafnyPrinter (Block ()) where
-  pp = withBrace . withIncr2 . (indent <>) . by (line <> indent) . inBlock
+  pp (Block b) = vsep
+    [ lbrace
+    , incr2 (vsep b)
+    , rbrace]
+
+
+instance (DafnyPrinter a, DafnyPrinter b) => DafnyPrinter (a :+: b) where
+  pp (Inl a) = pp a
+  pp (Inr b) = pp b
 
 instance DafnyPrinter (Toplevel ()) where
-  pp ty = case unTop ty of
-    Inl q -> pp q
-    Inr q -> pp q
+  pp = pp . unTop
 
+ppInvs :: [Exp'] -> Builder
+ppInvs = vsep . (("invariant" <+>) <$> )
 
 instance DafnyPrinter (Stmt ()) where
   pp (SEmit (SBlock b)) = pp b
   pp (SEmit f@(SForEmit idf initf bound invs b)) =
-    "for " <!> fromString idf <!> " := " <!> initf <!> " to " <!> bound
-      <!> "\n"
-      <!> withIncr2 (indent <> byLineT (("invariant" <+>) <$> invs))
-      <!> b
+    "for " <!> idf <!> " := " <!> initf <!> " to " <!> bound <!> line
+    <!> ppInvs invs
+    <!> b
 
   pp (SDafny s') = pp s'
 
   pp s@(SFor idx boundl boundr eG invs seps body) = debugOnly s $
-    "for"
-    <+> fromString idx <+> "∈" <+> withBracket (boundl <+> ".." <+> boundr)
-    <+> "with" <+> eG <!> line
-    <!> withIncr2 (indent <> byLineT (("invariant" <+>) <$> invs))
+    "for" <+> idx <+> "∈" <+> brackets (boundl <+> ".." <+> boundr)
+    <+> "with" <+> eG <!> (line :: Builder)
+    <!> ppInvs invs
     <!> body
 
   -- Statements that end with a SemiColon
   pp s@(SIf eg sep block) = debugOnly s $
-    "if" <+> withParen eg <!> line <!>
-    indent <!> "seperates" <+> sep <!> line <!>
-    block
+    "if" <+> parens eg <!> line
+    <!> incr2 ("seperates" <+> sep) <!> line
+    <!> block
 
   pp s = ppStmt s <> pp ';'
     where
       ppStmt :: Stmt' -> Builder
       ppStmt (SVar bd Nothing) = "var " <!> bd
       ppStmt (SVar bd (Just e)) = "var " <!> bd <!> " := " <!> e
-      ppStmt (v ::=: e) = fromString v <!> " := " <!> e
-      ppStmt (SCall v es) = fromString v <!> withParen (byComma es)
+      ppStmt (v ::=: e) = v <!> " := " <!> e
+      ppStmt (SCall v es) = v <!> tupled es
       ppStmt (SEmit s') = ppEmit s'
       ppStmt (SAssert e) = "assert " <!> e
       ppStmt (e1 :*=: e2) = debugOnly s $
         e1 <+> "*=" <+> λHuh e2
-      ppStmt e = "// undefined pper for Stmt : " <!> fromString (show e)
+      ppStmt e = "// undefined pper for Stmt : " <!> viaShow e
 
       λHuh e@(ELambda {}) = "λ" <+> e
       λHuh e              = pp e
@@ -244,7 +209,7 @@ instance DafnyPrinter (Stmt ()) where
       ppEmit (SVars bds e) = "var" <+> byComma bds <+> ":=" <+> e
       ppEmit (vs :*:=: rhs)  = case rhs of
         [] -> mempty
-        _  -> byComma (fromString <$> vs) <+> ":=" <+> byComma (withParen <$> rhs)
+        _  -> byComma vs <+> ":=" <+> byComma (parens <$> rhs)
       -- ppEmit (SIfDafny e b) = "if " <!> withParen (pp e) <!> b
       ppEmit _             = error "Should have been handled!!"
 
@@ -253,56 +218,55 @@ instance DafnyPrinter GuardExp where
 
 instance DafnyPrinter (Exp ()) where
   pp (ENum n) = pp n
-  pp (EVar v) = pp (fromString v)
+  pp (EVar v) = pp v
   pp (EBool b) = pp $ if b then "true" else "false"
   pp (EEmit e) = pp e
   pp (EOp1 ONeg e1) = "-" <+> e1
   pp (EOp2 op e1 e2) = ppOp2 op (pp e1) (pp e2)
   -- parentheses are critical to forall expressions!
   pp (EForall x eb e) =
-    withParen $ "forall " <!> x  <!> beb eb <!> " :: " <!> e
+    parens $ "forall " <!> x  <!> beb eb <!> " :: " <!> e
     where
       beb (Just eb') = " | " <!> eb'
       beb Nothing    = mempty
   pp e@EHad = debugOnly e "H"
   pp e@(ESpec p qt specs) = debugOnly e $
     "{" <+> p <+> ":" <+> qt  <+> "↦" <+> byComma specs <+> "}"
-  pp e@(EApp v es) = fromString v <!> withParen (byComma es)
+  pp e@(EApp v es) = v <!> tupled es
   pp e@(EMeasure s) = debugOnly e $
     "measure" <+> s
   pp EWildcard = pp "_"
   pp (ELambda el) = pp el
-  pp e = "//" <!> fromString (show e) <!> " should not be in emitted form!"
+  pp e = "//" <!> viaShow e <!> " should not be in emitted form!"
 
 instance (Show f, DafnyPrinter f) => DafnyPrinter (LambdaF f) where
   pp e@(LambdaF{ bPhase, bBases, ePhase, eBases }) =
     case (bPhase, ePhase) of
       (PhaseWildCard, PhaseWildCard) ->
-        tupled (fromString <$> bBases) <+> "=>" <+> tupled eBases
+        tupled bBases <+> "=>" <+> tupled eBases
       (_, _) -> debugOnly e $
         bPhase <+>
-        "~" <+> tupled (fromString <$> bBases) <+>
+        "~" <+> tupled bBases <+>
         "=>" <+>
         ePhase <+> "~" <+> tupled eBases
 
 instance DafnyPrinter Intv where
   pp e@(Intv e1 e2) = debugOnly e $
-    withBracket $ e1 <+> ".." <+> e2
+    brackets $ e1 <+> ".." <+> e2
 
 instance (DafnyPrinter f, Show f) => DafnyPrinter (SpecExpF f) where
   pp s = debugOnly s ppSubterm
     where
       ppSubterm = case s of
         SEWildcard -> pp "_"
-        SESpecNor (SpecNorF v1 e2) -> "⊗" <+> fromString v1 <+> '.' <+> e2
-        SESpecHad (SpecHadF v1 p) -> "⊗" <+> fromString v1 <+> '.' <+> p
+        SESpecNor (SpecNorF v1 e2) -> "⊗" <+> v1 <+> '.' <+> e2
+        SESpecHad (SpecHadF v1 p) -> "⊗" <+> v1 <+> '.' <+> p
         SESpecEn (SpecEnF v1 intv a p es) ->
-          "Σ" <+> fromString v1 <+> "∈" <+> intv <+> '.' <+> a <+> p <+>
-          withParen (byComma es)
+          "Σ" <+> v1 <+> "∈" <+> intv <+> '.' <+> a <+> p <+> tupled es
         SESpecEn01 (SpecEn01F v1 intv1 a p v2 intv2 e5) ->
-          "Σ" <+> fromString v1 <+> "∈" <+> intv1 <+> '.' <+>
-          "⊗" <+> fromString v2 <+> "∈" <+> intv2 <+> '.' <+>
-          a <+> p <+> withParen (byComma e5)
+          "Σ" <+> v1 <+> "∈" <+> intv1 <+> '.' <+>
+          "⊗" <+> v2 <+> "∈" <+> intv2 <+> '.' <+>
+          a <+> p <+> tupled e5
 
 instance DafnyPrinter f => DafnyPrinter (Maybe f) where
   pp Nothing  = pp "_"
@@ -312,15 +276,15 @@ instance (Show f, DafnyPrinter f) => DafnyPrinter (PhaseExpF f) where
   pp p = debugOnly p $ case p of
     PhaseZ                -> pp "1"
     PhaseWildCard         -> pp "_"
-    PhaseOmega e1 e2      -> "ω" <!> withParen (byComma [e1, e2])
-    PhaseSumOmega i e1 e2 -> "Ω" <+> i <+> "." <+> withParen (byComma [e1, e2])
+    PhaseOmega e1 e2      -> "ω" <!> tupled [e1, e2]
+    PhaseSumOmega i e1 e2 -> "Ω" <+> i <+> "." <+> tupled [e1, e2]
 
 instance (Show f, DafnyPrinter f) => DafnyPrinter (AmpExpF f) where
   pp p = debugOnly p $ case p of
     ADefault     -> mempty
-    AISqrt en ed -> "isqpp " <+> withParen (byComma [en, ed])
-    ASin e       -> "sin" <!> withParen (pp e)
-    ACos e       -> "cos" <!> withParen (pp e)
+    AISqrt en ed -> "isqpp " <+> tupled [en, ed]
+    ASin e       -> "sin" <!> parens e
+    ACos e       -> "cos" <!> parens e
 
 
 instance DafnyPrinter EmitExp where
@@ -328,36 +292,35 @@ instance DafnyPrinter EmitExp where
   pp (e1 :@@: (e2, e3)) = e1 <!> "[" <!> e2 <!> ".." <!> e3 <!> "]"
   pp EMtSeq = pp "[]"
   pp (EMakeSeq ty e ee) =
-    "seq<" <!> ty <!> ">" <!> withParen (e <!> ", " <!> ee)
-  pp (EDafnyVar s) = pp (fromString s)
+    "seq<" <!> ty <!> ">" <!> parens (e <!> ", " <!> ee)
+  pp (EDafnyVar s) = pp s
   pp (EOpChained e eos) =
     foldl (\el (op, er) -> ppOp2 op el (pp er)) (pp e) eos
   pp (ECard e) = "|" <!> e <!> "|"
-  pp (ECall v es) = fromString v <!> withParen (byComma es)
-  pp (EMultiLambda vs e) = withParen (byComma (fromString <$> vs)) <+> "=>" <+> e
+  pp (ECall v es) = v <!> tupled es
+  pp (EMultiLambda vs e) = tupled vs <+> "=>" <+> e
   pp (EAsReal e) =
-    withParen (withParen e <+> "as real")
+    parens (parens e <+> "as real")
 
 instance DafnyPrinter Range where
   pp rr@(Range v l r) = debugOnly rr $ pp (EVar v :@@: (l, r))
 
 instance DafnyPrinter Partition where
-  pp pp@(Partition p) = debugOnly pp $ byComma p
+  pp par@(Partition p) = debugOnly par $ byComma p
 
 instance DafnyPrinter Loc where
-  pp = pp . fromString . deref
+  pp = pp . deref
 
 instance DafnyPrinter Locus where
   pp st@(Locus {loc=l, part=p, qty, degrees=dgrs}) = debugOnly st $
-    l <+> "↦" <+> p <+> "::" <+> qty <+> withBracket (byComma dgrs)
-
+    l <+> "↦" <+> p <+> "::" <+> qty <+> list dgrs
 
 instance DafnyPrinter PhaseRef where
   pp PhaseRef{prBase, prRepr} =
     prRepr <+> "/" <+> prBase
 
 instance DafnyPrinter EmitData where
-  pp EmitData{evPhaseRef, evBasis, evAmp} = byComma $
+  pp EmitData{evPhaseRef, evBasis, evAmp} = list
     [ "phase:" <+> evPhaseRef
     , "ket:"   <+> evBasis
     , "amp:"   <+> evAmp
@@ -365,13 +328,13 @@ instance DafnyPrinter EmitData where
 
 
 instance (DafnyPrinter a, DafnyPrinter b) => DafnyPrinter (a, b) where
-  pp t'@(a, b) = withParen . byComma $ [pp a, pp b]
+  pp t'@(a, b) = tupled [pp a, pp b]
 
 instance ( DafnyPrinter a
          , DafnyPrinter b
          , DafnyPrinter c) => DafnyPrinter (a, b, c) where
   pp t'@(a, b, c) =
-    withParen . byComma $ [pp a, pp b, pp c]
+    tupled $ [pp a, pp b, pp c]
 
 -- instance (Show f, DafnyPrinter f) => DafnyPrinter (QSpecF f) where
 --   pp q@QSpecF{amp, phase, spec} = debugOnly q $
@@ -381,7 +344,7 @@ instance ( DafnyPrinter a
 instance ( DafnyPrinter k, DafnyPrinter v
          ) => DafnyPrinter (Map.Map k v) where
   pp m' = debugOnly' $
-    byLineT ((indent <>) . row <$> m)
+    list (row <$> m)
     where
       m = Map.toList m'
       row (a, b) = a <+> "↦" <+> b
@@ -395,20 +358,19 @@ instance DafnyPrinter MTy where
 -- on this function not to be parenthesized
 -- TODO: I want to get the precedence right here.
 ppOp2 :: Op2 -> Builder -> Builder -> Builder
-ppOp2 ONor b1 b2 = "nor" <!> withParen (byComma [b1, b2])
+ppOp2 ONor b1 b2 = "nor" <!> tupled [b1, b2]
 ppOp2 op b1 b2 =  parenOpt b1 <!> opSign <!> parenOpt b2
   where
     parenOpt :: Builder -> Builder
     parenOpt =
       case op of
-        OAnd -> withParen
-        OOr  -> withParen
-        OMod -> withParen -- mod is a fragile operator
-        ODiv -> withParen
+        OAnd -> parens
+        OOr  -> parens
+        OMod -> parens -- mod is a fragile operator
+        ODiv -> parens
         _    -> id
 
-    opSign :: TB.Builder
-    opSign = fromString $
+    opSign = pp $
       case op of
         OAnd -> " && "
         OOr  -> " || "
@@ -424,26 +386,30 @@ ppOp2 op b1 b2 =  parenOpt b1 <!> opSign <!> parenOpt b2
         OGe  -> " >= "
 
 ppConds :: DafnyPrinter a => a -> [Exp'] -> [Builder]
-ppConds s = map ((s <!> space) <!>)
+ppConds s = map ((s <+> (space :: Builder)) <!>)
 
-runBuilder :: DafnyPrinter a => Int -> Bool -> a -> Text
-runBuilder i debug = TB.toLazyText . flip runReader (i, debug) . doPp . pp
+runBuilder :: DafnyPrinter a => Int -> Bool -> a -> TL.Text
+runBuilder i debug =
+  renderLazy debug
+  . P.layoutPretty P.defaultLayoutOptions
+  . P.indent i
+  . pp
 
-texify :: DafnyPrinter a => a -> Text
+texify :: DafnyPrinter a => a -> TL.Text
 texify = runBuilder 0 False
 
 prettyIO :: DafnyPrinter a => a -> IO ()
-prettyIO = TIO.putStrLn . runBuilder 0 True
+prettyIO = putDoc False . pp
 
-showEmit :: DafnyPrinter a => a -> String
-showEmit = unpack . texify
+-- showEmit :: DafnyPrinter a => a -> String
+-- showEmit = TL.unpack . texify
 
--- Debug mode
-showEmitI :: DafnyPrinter a => Int -> a -> String
-showEmitI i = unpack . runBuilder i True
+-- -- Debug mode
+-- showEmitI :: DafnyPrinter a => Int -> a -> String
+-- showEmitI i = TL.unpack . runBuilder i True
 
-showEmit0 :: DafnyPrinter a => a -> String
-showEmit0 = showEmitI 0
+-- showEmit0 :: DafnyPrinter a => a -> String
+-- showEmit0 = showEmitI 0
 
 
 -- Regex for OverloadedStrings
@@ -454,18 +420,17 @@ showEmit0 = showEmitI 0
 --------------------------------------------------------------------------------
 
 instance DafnyPrinter [Int] where
-  pp = withBracket . byComma
+  pp = list
 
 instance DafnyPrinter TState where
-  pp TState{_sSt, _xSt, _emitSt} = byLineT $
+  pp TState{_sSt, _xSt, _emitSt} = vsep
     [ pp "Partition Reference State:"
-    , pp (byLineT <$> _xSt)
+    , ppincr2 (list <$> _xSt)
     , pp "Partition State:"
-    , pp _sSt
+    , ppincr2 _sSt
     , pp "Renaming State:"
-    , pp _emitSt
+    , ppincr2 _emitSt
     ]
-
-instance (DafnyPrinter a, DafnyPrinter b) => DafnyPrinter (a :+: b) where
-  pp (Inl a) = pp a
-  pp (Inr b) = pp b
+    where
+      ppincr2 :: DafnyPrinter a => a -> Builder
+      ppincr2 = pp . incr2
