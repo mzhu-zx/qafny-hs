8f9691b2af18734d35b459e9fb6f1685a7303491
Mingwei Zhu <mzhu1@terpmail.umd.edu>
2024-04-28 23:19:20 -0400

    [test] one more

--- test/Qafny/Test/EmitBinding.hs
+++ test/Qafny/Test/EmitBinding.hs
@@ -17,7 +17,7 @@ import           Qafny.Syntax.EmitBinding
     (Emitter)
 import           Qafny.Syntax.IR
 import           Qafny.Utils.EmitBinding
-    (genEmStFromLocus)
+    (genEmStFromLocus, regenEmStByLocus)
 
 
 ppTerm :: DafnyPrinter a => (Int, [(Emitter, String)], (TState, Either Builder a)) -> IO ()
@@ -33,3 +33,15 @@ term1 = debugGensymEmitterWithState . runError $
     l :: Locus
     l = Locus (Loc "a") (normalize (Partition rs)) TEn [0]
     rs = [Range "x" 0 1, Range "y" 0 1]
+
+term2 :: (Int, [(Emitter, String)], (TState, Either Builder _))
+term2 = debugGensymEmitterWithState . runError . (LocusEmitData' <$>) $
+   genEmStFromLocus l1 >> regenEmStByLocus l1 l2
+  where
+    l1 :: Locus
+    l1 = Locus (Loc "a") (normalize (Partition rs1)) TEn [0]
+    rs1 = [Range "x" 0 1, Range "y" 0 1]
+    --
+    l2 :: Locus
+    l2 = Locus (Loc "a") (normalize (Partition rs2)) THad [0]
+    rs2 = [Range "x" 0 1]
